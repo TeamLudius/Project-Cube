8dcde6e81c283ce0fa578e4c60f81dca4e50b198
ludiusdev <aevar.kristjansson@gmail.com>
2017-07-01 00:11:16 +0000

    Fixes, tests, and add ons
    -Shop test
    -New skin (Blackout)
    -Pause button fixed on higher levels
    -Reworked level 7

---
+++ Game/Assets/Scripts/ShopTest02.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopTest02 : MonoBehaviour {
+
+	public GameObject RedCube;
+	public GameObject Test02;
+	public GameObject Test03;
+
+	public void Test ()
+	{
+		var RandomValue = Random.value;
+		//0 - .5
+		//.5 - .7
+		//.7 - 1
+
+		if(RandomValue > 0 && RandomValue <= 0.5) //%50 percent chance
+			{
+			RedCube.SetActive (true);
+			Invoke("TurnOff01", 10f);
+		}
+
+		if (RandomValue > 0.5 && RandomValue <= 0.7) //%80 percent chance (1 - 0.2 is 0.8)
+		{
+			Test02.SetActive (true);
+			Invoke("TurnOff02", 10f);
+		}
+
+		if(RandomValue > 0.7 && RandomValue <= 1) //%30 percent chance (1 - 0.7 is 0.3)
+		{ 
+			Test03.SetActive (true);
+			Invoke("TurnOff03", 10f);
+		}
+
+	}
+
+	void TurnOff01 ()
+	{
+		RedCube.SetActive (false);
+	}
+
+	void TurnOff02 ()
+	{
+		Test02.SetActive (false);
+	}
+
+	void TurnOff03 ()
+	{
+		Test03.SetActive (false);
+	}
+}
